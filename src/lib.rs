a789695d19cfb749561ef05cddf18427a40a7ed8
jkosha <nikita.koselev@gmail.com>
2023-01-20 21:03:32 +0000

    tests are green

--- src/lib.rs
+++ src/lib.rs
@@ -11,14 +11,14 @@ pub enum LogLevel {
 }
 /// primary function for emitting logs
 pub fn log(level: LogLevel, message: &str) -> String {
-    unimplemented!("return a message for the given log level")
+    format!("[{}]: {}", format!("{:?}", level).to_uppercase(), message)
 }
 pub fn info(message: &str) -> String {
-    unimplemented!("return a message for info log level")
+    log(LogLevel::Info, message)
 }
 pub fn warn(message: &str) -> String {
-    unimplemented!("return a message for warn log level")
+    log(LogLevel::Warning, message)
 }
 pub fn error(message: &str) -> String {
-    unimplemented!("return a message for error log level")
+    log(LogLevel::Error, message)
 }
